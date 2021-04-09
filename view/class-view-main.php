689c43ef6b2c56fdba7b710fdcdab7407d3572e7
Seba Rossi <sebastopolys@users.noreply.github.com>
2021-04-09 10:44:17 -0300

    Add files via upload

---
+++ view/class-view-main.php
@@ -0,0 +1,19 @@
+<?php
+require_once (HOMPATH.'/view/templates/header.php');
+require_once (HOMPATH.'/view/templates/form.php');
+require_once (HOMPATH.'/view/templates/debug.php');
+require_once (HOMPATH.'/view/templates/client.php');
+require_once (HOMPATH.'/view/templates/footer.php');
+class view_main{
+    public function __construct($state,$user_dat,$user_lic){  
+        echo print_head(PREFIX,MAINAME,DESCR);       
+        echo print_form();
+
+        if(DEBUG==true){
+        echo print_debug();
+        }           
+        if(isset($state)){
+            echo print_client($user_dat,$user_lic);
+        }              
+    }
+}
